f1e3b98638d0eefa8055efd7d24cdd0682822f52
SrjQA <surajpandkar2000@gmail.com>
2026-05-10 17:33:53 +0530

    added file

---
+++ test1.java
@@ -0,0 +1,10 @@
+package JAVA_PRACTICE;
+
+public class test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       System.out.println("Hello Suraj");
+	}
+
+}
